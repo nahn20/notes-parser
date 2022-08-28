aca39fe0089f27c5cfc6d9d68d248432fc64d6ed
Nathan Ahn <nathancahn@gmail.com>
2022-08-28 16:22:47 -0400

    Fixed

--- updateFromRepo.sh
+++ updateFromRepo.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 SCRIPTPATH=$(dirname "$0")
 
-cd $SCRIPTPATH &&
-git pull &&
-pip3 install -r requirements.txt
+(cd $SCRIPTPATH; git pull) &&
+pip3 install -r "$SCRIPTPATH/requirements.txt"
